bfac74b70e265d5450a0dcedf411a9c3b6bc3f06
imdavyking <yungdynamic53@gmail.com>
2024-10-07 13:07:11 +0100

    added xion chainInfo

--- utils/constants.ts
+++ utils/constants.ts
@@ -45,57 +45,57 @@ export const appMetaData = {
 
 export const TIME_TILL_LOCK = 15 * 60 * 1000;
 
-// export const cosmosChainInfo = {
-//   chainId: "xion-testnet-1",
-//   chainName: "xion-testnet-1",
-//   rpc: "https://rpc.xion-testnet-1.burnt.com",
-//   rest: "https://api.xion-testnet-1.burnt.com",
-//   bip44: {
-//     coinType: 118,
-//   },
-//   coinType: 118,
-//   bech32Config: {
-//     bech32PrefixAccAddr: "xion",
-//     bech32PrefixAccPub: "xionpub",
-//     bech32PrefixValAddr: "xionvaloper",
-//     bech32PrefixValPub: "xionvaloperpub",
-//     bech32PrefixConsAddr: "xionvalcons",
-//     bech32PrefixConsPub: "xionvalconspub",
-//   },
-//   currencies: [
-//     {
-//       coinDenom: "XION",
-//       coinMinimalDenom: "uxion",
-//       coinDecimals: 6,
-//       coinGeckoId: "xion",
-//     },
-//   ],
-//   feeCurrencies: [
-//     {
-//       coinDenom: "XION",
-//       coinMinimalDenom: "uxion",
-//       coinDecimals: 6,
-//       coinGeckoId: "xion",
-//       gasPriceStep: {
-//         low: 0.01,
-//         average: 0.025,
-//         high: 0.03,
-//       },
-//     },
-//   ],
-//   gasPriceStep: {
-//     low: 0.01,
-//     average: 0.025,
-//     high: 0.03,
-//   },
-//   stakeCurrency: {
-//     coinDenom: "XION",
-//     coinMinimalDenom: "uxion",
-//     coinDecimals: 6,
-//     coinGeckoId: "xion",
-//   },
-//   features: ["ibc-go", "ibc-transfer", "no-legacy-stdTx"],
-// };
+export const xionChainInfo = {
+  chainId: "xion-testnet-1",
+  chainName: "xion-testnet-1",
+  rpc: "https://rpc.xion-testnet-1.burnt.com",
+  rest: "https://api.xion-testnet-1.burnt.com",
+  bip44: {
+    coinType: 118,
+  },
+  coinType: 118,
+  bech32Config: {
+    bech32PrefixAccAddr: "xion",
+    bech32PrefixAccPub: "xionpub",
+    bech32PrefixValAddr: "xionvaloper",
+    bech32PrefixValPub: "xionvaloperpub",
+    bech32PrefixConsAddr: "xionvalcons",
+    bech32PrefixConsPub: "xionvalconspub",
+  },
+  currencies: [
+    {
+      coinDenom: "XION",
+      coinMinimalDenom: "uxion",
+      coinDecimals: 6,
+      coinGeckoId: "xion",
+    },
+  ],
+  feeCurrencies: [
+    {
+      coinDenom: "XION",
+      coinMinimalDenom: "uxion",
+      coinDecimals: 6,
+      coinGeckoId: "xion",
+      gasPriceStep: {
+        low: 0.01,
+        average: 0.025,
+        high: 0.03,
+      },
+    },
+  ],
+  gasPriceStep: {
+    low: 0.01,
+    average: 0.025,
+    high: 0.03,
+  },
+  stakeCurrency: {
+    coinDenom: "XION",
+    coinMinimalDenom: "uxion",
+    coinDecimals: 6,
+    coinGeckoId: "xion",
+  },
+  features: ["ibc-go", "ibc-transfer", "no-legacy-stdTx"],
+};
 
 export const cosmosChainInfo = {
   // Chain-id of the Osmosis chain.
